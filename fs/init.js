8374c551229bb673d7f560f92ab53e6039b1424b
John Knutson <john.knutson@novu.com>
2017-12-02 22:31:49 -0600

    add onewire bits

--- fs/init.js
+++ fs/init.js
@@ -4,12 +4,43 @@ load('api_mqtt.js');
 load('api_net.js');
 load('api_sys.js');
 load('api_timer.js');
+load('api_arduino_onewire.js');
+load('ds18b20.js');
 
+let oneWirePin = 14;
 let led = Cfg.get('pins.led');
 let button = Cfg.get('pins.button');
 let topic = '/devices/' + Cfg.get('device.id') + '/events';
 
 print('LED GPIO:', led, 'button GPIO:', button);
+
+// Initialize OneWire library
+let ow = OneWire.create(oneWirePin);
+// Number of sensors found on the 1-Wire bus
+let n = 0;
+// Sensors addresses
+let rom = ['01234567'];
+
+// Search for sensors
+let searchSens = function() {
+  let i = 0;
+  // Setup the search to find the device type on the next call
+  // to search() if it is present.
+  ow.target_search(DEVICE_FAMILY.DS18B20);
+
+  while (ow.search(rom[i], 0/* Normal search mode */) === 1) {
+    // If no devices of the desired family are currently on the bus, 
+    // then another type will be found. We should check it.
+    if (rom[i][0].charCodeAt(0) !== DEVICE_FAMILY.DS18B20) {
+      break;
+    }
+    // Sensor found
+    print('Sensor#', i, 'address:', toHexStr(rom[i]));
+    rom[++i] = '01234567';
+  }
+  return i;
+};
+
 
 let getInfo = function() {
   return JSON.stringify({
@@ -23,6 +54,29 @@ GPIO.set_mode(led, GPIO.MODE_OUTPUT);
 Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
   let value = GPIO.toggle(led);
   print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime(), getInfo());
+  if (n === 0) {
+    if ((n = searchSens()) === 0) {
+      print('No device found');
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    let t = getTemp(ow, rom[i]);
+    if (isNaN(t)) {
+      print('No device found');
+      break;
+    } else {
+      print('Sensor#', i, 'Temperature:', t, '*C');
+      let f_t = t * 9 / 5 + 32;
+      let message = JSON.stringify({
+        fahrenheit: f_t,
+        celcius: t
+      });
+      let ok = MQTT.pub(topic, message, 1);
+      print('Published:', ok, topic, '->', message)
+    }
+  }
+
 }, null);
 
 // Publish to MQTT topic on a button press. Button is wired to GPIO pin 0
