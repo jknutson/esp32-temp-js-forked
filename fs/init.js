6f59344d51430595f13f86f62e69de26ef05dee2
John Knutson <john.knutson@novu.com>
2017-12-03 12:40:35 -0600

    revert back to working timer loop

--- fs/init.js
+++ fs/init.js
@@ -102,32 +102,8 @@ Timer.set(30000, true, function() {
 
 }, null);
 
-// let lastDoorStatus = 1; // 1=closed
 Timer.set(5000 , true , function() {
-//   doorStatus = GPIO.read(doorPin);
-//   let doorEvent = 'null';
-//   if (doorStatus !== lastDoorStatus) {
-//     // door state change
-//     if (doorStatus) {
-//       // door was open, is now closed
-//       doorEvent = 'doorClosed';
-//     } else {
-//       doorEvent = 'doorOpened';
-//     }
-//     let message = JSON.stringify({
-//       data: {
-//         doorEvent: doorEvent,
-//         doorStatus: doorStatus,
-//         pin: doorPin,
-//         timestamp: timestamp()
-//       },
-//       deviceId: deviceId,
-//       deviceType: deviceType
-//     });
-//     let ok = MQTT.pub(topic, message, 1);
-//     print('Published:', ok, topic, '->', message);
-//     lastDoorStatus = doorStatus;
-//   }
+  let doorStatus = GPIO.read(doorPin);
   let message = JSON.stringify({
     data: {
       doorStatus: doorStatus,
