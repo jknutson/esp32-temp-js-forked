3fa244deffc7a87af365a329160b5aa1dad784ac
John Knutson <john.knutson@novu.com>
2017-12-03 01:40:13 -0600

    update timeouts, add deviceType

--- fs/init.js
+++ fs/init.js
@@ -9,40 +9,30 @@ load('api_arduino_onewire.js');
 load('ds18b20.js');
 
 let timeFormat = '%FT%T%z';
-let doorPin = Cfg.get('pins.door');
-let oneWirePin = Cfg.get('pins.temp');
-let builtinPin = Cfg.get('pins.builtin');
-print(doorPin);
-print(oneWirePin);
-print(builtinPin);
+// TODO make configs work
 let doorPin = 34;
 let oneWirePin = 14;
 let builtinPin = 0;
 let humidPin = 33;
-let topic = '/devices/' + Cfg.get('device.id') + '/events';
+let deviceId = Cfg.get('device.id');
+let deviceType = 'esp32';
+let topic = '/devices/' + deviceId + '/events';
 
-// Initialize OneWire library
 let ow = OneWire.create(oneWirePin);
-// Number of sensors found on the 1-Wire bus
 let n = 0;
-// Sensors addresses
 let rom = ['01234567'];
 
-// Search for sensors
+GPIO.set_mode(doorPin, GPIO.MODE_INPUT);
+
 let searchSens = function() {
   let i = 0;
-  // Setup the search to find the device type on the next call
-  // to search() if it is present.
   ow.target_search(DEVICE_FAMILY.DS18B20);
 
   while (ow.search(rom[i], 0/* Normal search mode */) === 1) {
-    // If no devices of the desired family are currently on the bus, 
-    // then another type will be found. We should check it.
     if (rom[i][0].charCodeAt(0) !== DEVICE_FAMILY.DS18B20) {
       break;
     }
     // Sensor found
-    print('Sensor#', i, 'address:', toHexStr(rom[i]));
     rom[++i] = '01234567';
   }
   return i;
@@ -59,24 +49,8 @@ let getInfo = function() {
   });
 };
 
-GPIO.set_mode(doorPin, GPIO.MODE_INPUT);
-/*
-{
-  "data": {
-    "loop": 1,
-    "sensor": "legopi",
-    "temperature": {
-      "celcius": 0.125,
-      "fahrenheit": 32.225
-    },
-    "timestamp": "2017-12-01T23:31:27.104806"
-  },
-  "device_id": "legopi",
-  "timestamp": "2017-12-01T23:31:27.104806"
-}
-*/
-//Timer.set(60000 /* 1 min */, true /* repeat */, function() {
-Timer.set(1000 /* 1 min */, true /* repeat */, function() {
+// temperature/humidity loop, 1000=1s,30000=30s,  60000=1m
+Timer.set(30000, true, function() {
   if (n === 0) {
     if ((n = searchSens()) === 0) {
       print('No device found');
@@ -89,7 +63,6 @@ Timer.set(1000 /* 1 min */, true /* repeat */, function() {
       print('No device found');
       break;
     } else {
-      print('Sensor#', i, 'Temperature:', t, '*C');
       let f_t = t * 9 / 5 + 32;
       let message = JSON.stringify({
         data: {
@@ -98,7 +71,9 @@ Timer.set(1000 /* 1 min */, true /* repeat */, function() {
             fahrenheit: f_t,
             celcius: t,
             timestamp: timestamp()
-          }
+          },
+          deviceId: deviceId,
+          deviceType: deviceType
         }
       });
       let ok = MQTT.pub(topic, message, 1);
@@ -108,32 +83,40 @@ Timer.set(1000 /* 1 min */, true /* repeat */, function() {
 
   // humidity
   let enableAnalog = ADC.enable(humidPin);
-  print(enableAnalog);
   let relativeHumidity = ADC.read(humidPin);
-  print(relativeHumidity);
+  // TODO: this math most likely needs adjustment
+  // see: http://www.instructables.com/id/HIH4000-Humidity-Hygrometer-Sensor-Tutorial/
   let av = 0.0048875*relativeHumidity;
-  print(av);
   let res = (av - 0.86) / 0.03;
-  print(res);
+  let message = JSON.stringify({
+    data: {
+      sensor: 'HIH4000',
+      humidity: res,
+      timestamp: timestamp()
+    },
+    deviceId: deviceId,
+    deviceType: deviceType
+  });
+  let ok = MQTT.pub(topic, message, 1);
+  print('Published:', ok, topic, '->', message)
 
 }, null);
 
-
-Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
-  // door stuff
+Timer.set(5000 , true , function() {
   let doorStatus = GPIO.read(doorPin);
   let message = JSON.stringify({
     data: {
       doorStatus: doorStatus,
       pin: doorPin,
       timestamp: timestamp()
-    }
+    },
+    deviceId: deviceId,
+    deviceType: deviceType
   });
   let ok = MQTT.pub(topic, message, 1);
   print('Published:', ok, topic, '->', message)
 }, null);
 
-// Monitor network connectivity.
 Net.setStatusEventHandler(function(ev, arg) {
   let evs = '???';
   if (ev === Net.STATUS_DISCONNECTED) {
