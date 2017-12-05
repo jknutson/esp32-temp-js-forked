db526a7264b7d833a6479357456c48b835377cf8
John Knutson <john.knutson@novu.com>
2017-12-04 22:15:08 -0600

    crank up resolution, we get 25gb of dynamo fo free! plus implemented TTL so even safer there...

--- fs/init.js
+++ fs/init.js
@@ -12,18 +12,21 @@ let timeFormat = '%FT%T%z';
 // TODO make configs work
 // let doorPin = 34;
 let doorPin = 4;
+let doorPinNO = 16;
 let oneWirePin = 14;
 let builtinPin = 0;
 let humidPin = 33;
 let deviceId = Cfg.get('device.id');
 let deviceType = 'esp32';
 let topic = '/devices/' + deviceId + '/events';
+let ttl = 604800; // one week in seconds
 
 let ow = OneWire.create(oneWirePin);
 let n = 0;
 let rom = ['01234567'];
 
 GPIO.set_mode(doorPin, GPIO.MODE_INPUT);
+GPIO.set_mode(doorPinNO, GPIO.MODE_INPUT);
 
 let searchSens = function() {
   let i = 0;
@@ -43,6 +46,11 @@ let timestamp = function() {
   return Timer.fmt(timeFormat, Timer.now());
 };
 
+let timestamp_ttl = function() {
+  let now = Timer.now();
+  return [Timer.fmt(timeFormat, Timer.now()), now + ttl];
+};
+
 let getInfo = function() {
   return JSON.stringify({
     total_ram: Sys.total_ram(),
@@ -51,7 +59,7 @@ let getInfo = function() {
 };
 
 // temperature/humidity loop, 1000=1s,30000=30s,  60000=1m
-Timer.set(30000, true, function() {
+Timer.set(1000, true, function() {
 
   // temperature
   if (n === 0) {
@@ -65,6 +73,7 @@ Timer.set(30000, true, function() {
       print('No device found');
       break;
     } else {
+      let ts = timestamp_ttl();
       let f_t = t * 9 / 5 + 32;
       let message = JSON.stringify({
         sensor: i,
@@ -72,7 +81,8 @@ Timer.set(30000, true, function() {
           fahrenheit: f_t,
           celcius: t
         },
-        timestamp: timestamp(),
+        timestamp: ts[0],
+        expires: ts[1],
         deviceId: deviceId,
         deviceType: deviceType,
         eventType: 'temperature'
@@ -89,10 +99,12 @@ Timer.set(30000, true, function() {
   // see: http://www.instructables.com/id/HIH4000-Humidity-Hygrometer-Sensor-Tutorial/
   let av = 0.0048875*relativeHumidity;
   let res = (av - 0.86) / 0.03;
+  let ts = timestamp_ttl();
   let message = JSON.stringify({
     sensor: 'HIH4000',
-    humidity: res
-    timestamp: timestamp(),
+    humidity: res,
+    timestamp: ts[0],
+    expires: ts[1],
     deviceId: deviceId,
     deviceType: deviceType,
     eventType: 'humidity'
@@ -102,12 +114,14 @@ Timer.set(30000, true, function() {
 
 }, null);
 
-Timer.set(5000 , true , function() {
+Timer.set(1000 , true , function() {
   let doorStatus = GPIO.read(doorPin);
+  let ts = timestamp_ttl();
   let message = JSON.stringify({
     doorStatus: doorStatus,
-    pin: doorPin
-    timestamp: timestamp(),
+    pin: doorPin,
+    timestamp: ts[0],
+    expires: ts[1],
     deviceId: deviceId,
     deviceType: deviceType,
     eventType: 'door'
@@ -115,6 +129,40 @@ Timer.set(5000 , true , function() {
   let ok = MQTT.pub(topic, message, 1);
   print('Published:', ok, topic, '->', message)
 }, null);
+
+
+GPIO.set_button_handler(doorPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 1000, function(x) {
+  let doorStatus = GPIO.read(doorPin);
+  let ts = timestamp_ttl();
+  let message = JSON.stringify({
+    doorStatus: doorStatus,
+    pin: x,
+    timestamp: ts[0],
+    expires: ts[1],
+    deviceId: deviceId,
+    deviceType: deviceType,
+    eventType: 'doorEvent'
+  });
+  let ok = MQTT.pub(topic, message, 1);
+  print('Published:', ok, topic, '->', message);
+}, null);
+
+// this isnt working, need some schematics help with my inverter
+// GPIO.set_button_handler(doorPinNO, GPIO.PULL_DOWN, GPIO.INT_EDGE_NEG, 200, function(x) {
+//   let doorStatus = GPIO.read(doorPinNO);
+//   let message = JSON.stringify({
+//     doorStatus: doorStatus,
+//     pin: x,
+//     timestamp: ts[0],
+//     deviceId: deviceId,
+//     deviceType: deviceType,
+//     eventType: 'doorEvent',
+//     eventValue: 'doorOpened'
+//   });
+//   let ok = MQTT.pub(topic, message, 1);
+//   print('Published:', ok, topic, '->', message);
+// }, null);
+
 
 Net.setStatusEventHandler(function(ev, arg) {
   let evs = '???';
