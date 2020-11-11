761318cb9c4962fb7e7fbd1ad38c1823a52a1295
John M. Knutson <john.knutson@novu.com>
2020-11-11 12:23:29 -0600

    switch to dallas temp library

--- fs/init.js
+++ fs/init.js
@@ -1,11 +1,11 @@
-load('api_adc.js');
+load('api_arduino_dallas_temp.js');
 load('api_arduino_onewire.js');
 load('api_config.js');
+// load('api_ds18b20.js');
 load('api_gpio.js');
 load('api_http.js');
-load('api_net.js');
+// load('api_net.js');
 load('api_timer.js');
-load('api_ds18b20.js');
 
 let deviceId = Cfg.get('device.id');
 let oneWirePin = Cfg.get('pins.temp');
@@ -15,8 +15,10 @@ let deviceType = 'esp32';
 let datadogApiKey = Cfg.get('datadog.api_key');
 
 let ow = OneWire.create(oneWirePin);
+let dt = DallasTemperature.create(ow);
+dt.begin();
 let n = 0;
-let rom = ['01234567'];
+let sens = [];
 
 print('deviceId:', deviceId)
 print('oneWirePin:', oneWirePin)
@@ -50,17 +52,31 @@ GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 50, function
 
 
 Timer.set(pollInterval, true, function() {
-  if (DS18B20.connected()) {
-    let t = DS18B20.get();
-    if (isNaN(t)) {
-      print('could not read from device: ' + t);
-      break;
-    } else {
+
+  if (n === 0) {
+    n = dt.getDeviceCount();
+    print('Sensors found:', n);
+
+    for (let i = 0; i < n; i++) {
+      sens[i] = '01234567';
+      if (dt.getAddress(sens[i], i) === 1) {
+        print('Sensor#', i, 'address:', dt.toHexStr(sens[i]));
+      }
+    }
+  }
+
+  if (n === 0) {
+    print('no sensors found')
+  } else {
+    dt.requestTemperatures();
+    for (let i = 0; i < n; i++) {
+      let tempC = dt.getTempC(sens[i]);
+      print('Sensor#', i, 'Temperature:', tempC, '*C');
       let payload = {
         series: [
           {
             metric: 'w1_temperature.celcius.gauge',
-            points: [[Timer.now(), t]],
+            points: [[Timer.now(), tempC]],
             tags: ['device:' + deviceId, 'deviceType:' + deviceType],
             type: 'gauge'
           }
@@ -78,7 +94,5 @@ Timer.set(pollInterval, true, function() {
         }
       });
     }
-  } else {
-    print('no oneWire device found')
   }
 }, null);
