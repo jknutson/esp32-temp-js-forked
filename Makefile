dea11b0c3fcf318ff11fa4322eb162a36d794a84
John M. Knutson <john.knutson@novu.com>
2020-11-12 22:57:23 -0600

    misc updates

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
 MOS=mos
 
-configure:
-	$(MOS) wifi "${WIFI_SSID}" "${WIFI_PASS}"
-	$(MOS) put combined.pem ca.pem
-	$(MOS) config-set datadog.api_key="${DD_API_KEY}"
-
 build:
 	$(MOS) build
 
 flash:
 	$(MOS) flash
+
+configure:
+	$(MOS) wifi "${WIFI_SSID}" "${WIFI_PASS}"
+	$(MOS) put combined.pem ca.pem
+	$(MOS) config-set datadog.api_key="${DD_API_KEY}"
+
+reboot:
+	$(MOS) call Sys.Reboot
+
+all: build flash configure reboot
--- fs/init.js
+++ fs/init.js
@@ -5,13 +5,15 @@ load('api_gpio.js');
 load('api_http.js');
 load('api_net.js');
 load('api_timer.js');
+load('api_sys.js');
 load('api_ds18b20.js');
 
 let deviceId = Cfg.get('device.id');
+let deviceType = 'esp32';
+let metricTags = ['device:' + deviceId, 'deviceType:' + deviceType];
 let oneWirePin = Cfg.get('pins.temp');
 let buttonPin = 0;  // builtin
 let pollInterval = Cfg.get('interval') * 1000;
-let deviceType = 'esp32';
 let datadogApiKey = Cfg.get('datadog.api_key');
 
 let ow = OneWire.create(oneWirePin);
@@ -20,6 +22,20 @@ let rom = ['01234567'];
 
 print('deviceId:', deviceId)
 print('oneWirePin:', oneWirePin)
+
+let postMetric = function(datadogApiKey, payload) {
+  print('publishing: ' + JSON.stringify(payload))
+  HTTP.query({
+    url: 'https://api.datadoghq.com/api/v1/series?api_key=' + datadogApiKey,
+    data: payload,
+    success: function(body, full_http_msg) {
+      print('datadog post metric success:', body);
+    },
+    error: function(err) {
+      print('datadog post metric error:', err);
+    }
+  });
+};
 
 GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 50, function(x) {
   let buttonPinString = JSON.stringify(buttonPin);
@@ -50,6 +66,29 @@ GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 50, function
 
 
 Timer.set(pollInterval, true, function() {
+  let totalRam = Sys.total_ram();
+  let freeRam = Sys.free_ram();
+
+  let now = Timer.now();
+  let sysPayload = {
+    series: [
+      {
+        metric: 'mos.sys.total_ram',
+        points: [[now, totalRam]],
+        tags: metricTags,
+        type: 'gauge'
+      },
+      {
+        metric: 'mos.sys.free_ram',
+        points: [[now, freeRam]],
+        tags: metricTags,
+        type: 'gauge'
+      }
+    ]
+  };
+  print('publishing: ' + JSON.stringify(sysPayload))
+  postMetric(datadogApiKey, sysPayload);
+
   if (DS18B20.connected()) {
     let t = DS18B20.get();
     if (isNaN(t)) {
@@ -61,22 +100,13 @@ Timer.set(pollInterval, true, function() {
           {
             metric: 'w1_temperature.celcius.gauge',
             points: [[Timer.now(), t]],
-            tags: ['device:' + deviceId, 'deviceType:' + deviceType],
+            tags: metricTags,
             type: 'gauge'
           }
         ]
       };
       print('publishing: ' + JSON.stringify(payload))
-      HTTP.query({
-        url: 'https://api.datadoghq.com/api/v1/series?api_key=' + datadogApiKey,
-        data: payload,
-        success: function(body, full_http_msg) {
-          print('datadog post metric success:', body);
-        },
-        error: function(err) {
-          print('datadog post metric error:', err);
-        }
-      });
+      postMetric(datadogApiKey, payload);
     }
   } else {
     print('no oneWire device found')
