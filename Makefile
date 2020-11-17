e5e568fa41826359bac4b6c18197db2f04523a9f
John Knutson <john.m.knutson@gmail.com>
2020-11-17 12:01:34 -0600

    emit host_name, other cleanup (#5)
    * misc updates
    
    * add hostname to config/datadog
    
    * host name updaets
    
    Co-authored-by: John M. Knutson <john.knutson@novu.com>

--- Makefile
+++ Makefile
@@ -1,12 +1,19 @@
 MOS=mos
 
-configure:
-	$(MOS) wifi "${WIFI_SSID}" "${WIFI_PASS}"
-	$(MOS) put combined.pem ca.pem
-	$(MOS) config-set datadog.api_key="${DD_API_KEY}"
+.PHONY: all build flash configure reboot
 
 build:
 	$(MOS) build
 
 flash:
 	$(MOS) flash
+
+configure:
+	$(MOS) wifi "${WIFI_SSID}" "${WIFI_PASS}"
+	$(MOS) put combined.pem ca.pem
+	$(MOS) config-set datadog.api_key="${DD_API_KEY}"
+
+reboot:
+	$(MOS) call Sys.Reboot
+
+all: build flash configure reboot
--- fs/init.js
+++ fs/init.js
@@ -1,18 +1,22 @@
 load('api_adc.js');
 load('api_arduino_onewire.js');
 load('api_config.js');
+load('api_events.js');
 load('api_gpio.js');
 load('api_http.js');
 load('api_net.js');
 load('api_timer.js');
+load('api_sys.js');
 load('api_ds18b20.js');
 
 let deviceId = Cfg.get('device.id');
+let deviceType = 'esp32';
+let metricTags = ['device:' + deviceId, 'deviceType:' + deviceType];
 let oneWirePin = Cfg.get('pins.temp');
 let buttonPin = 0;  // builtin
 let pollInterval = Cfg.get('interval') * 1000;
-let deviceType = 'esp32';
 let datadogApiKey = Cfg.get('datadog.api_key');
+let datadogHostName = Cfg.get('datadog.host_name');
 
 let ow = OneWire.create(oneWirePin);
 let n = 0;
@@ -21,14 +25,27 @@ let rom = ['01234567'];
 print('deviceId:', deviceId)
 print('oneWirePin:', oneWirePin)
 
+let postMetric = function(datadogApiKey, payload) {
+  print('publishing: ' + JSON.stringify(payload))
+  HTTP.query({
+    url: 'https://api.datadoghq.com/api/v1/series?api_key=' + datadogApiKey,
+    data: payload,
+    success: function(body, full_http_msg) {
+      print('datadog post metric success:', body);
+    },
+    error: function(err) {
+      print('datadog post metric error:', err);
+    }
+  });
+};
+
 GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 50, function(x) {
   let buttonPinString = JSON.stringify(buttonPin);
   // TODO: list sensors and include in event payload, plus any other debugging info?
   let payload = JSON.stringify({
     text: 'button on pin ' + buttonPinString + ' was pressed',
     title: 'esp32 device button pressed',
-    device_name: deviceId,
-    host: deviceId,
+    host: datadogHostName,
     tags: [
       'device:' + deviceId,
       'deviceType:' + deviceType,
@@ -50,6 +67,31 @@ GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 50, function
 
 
 Timer.set(pollInterval, true, function() {
+  let totalRam = Sys.total_ram();
+  let freeRam = Sys.free_ram();
+
+  let now = Timer.now();
+  let sysPayload = {
+    series: [
+      {
+        metric: 'mos.sys.total_ram',
+        points: [[now, totalRam]],
+        host: datadogHostName,
+        tags: metricTags,
+        type: 'gauge'
+      },
+      {
+        metric: 'mos.sys.free_ram',
+        points: [[now, freeRam]],
+        host: datadogHostName,
+        tags: metricTags,
+        type: 'gauge'
+      }
+    ]
+  };
+  print('publishing: ' + JSON.stringify(sysPayload))
+  postMetric(datadogApiKey, sysPayload);
+
   if (DS18B20.connected()) {
     let t = DS18B20.get();
     if (isNaN(t)) {
@@ -61,24 +103,24 @@ Timer.set(pollInterval, true, function() {
           {
             metric: 'w1_temperature.celcius.gauge',
             points: [[Timer.now(), t]],
-            tags: ['device:' + deviceId, 'deviceType:' + deviceType],
+            host: datadogHostName,
+            tags: metricTags,
             type: 'gauge'
           }
         ]
       };
       print('publishing: ' + JSON.stringify(payload))
-      HTTP.query({
-        url: 'https://api.datadoghq.com/api/v1/series?api_key=' + datadogApiKey,
-        data: payload,
-        success: function(body, full_http_msg) {
-          print('datadog post metric success:', body);
-        },
-        error: function(err) {
-          print('datadog post metric error:', err);
-        }
-      });
+      postMetric(datadogApiKey, payload);
     }
   } else {
     print('no oneWire device found')
   }
+}, null);
+
+Event.addGroupHandler(Net.STATUS_GOT_IP, function(ev, evdata, ud) {
+  // char *mgos_net_ip_to_str(const struct sockaddr_in *sin, char *out);
+  // let f = ffi('int my_func(int, int)');
+  // print('Calling C my_func:', f(1,2));
+  let gotIp =
+  print('== Net event:', 'GOT_IP', ev, evdata, ud);
 }, null);
